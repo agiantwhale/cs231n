4e30cba1ecd13b42f12e6f7e8f656e80730d0b97
Il Jae Lee <iljae@umich.edu>
2017-05-12 22:13:50 +0900

    Fixed adam

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -140,7 +140,12 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
+    config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dx**2)
+    config['m'] /= (1 - config['beta1'] ** config['t'])
+    config['v'] /= (1 - config['beta2'] ** config['t'])
+    next_x = x - config['learning_rate'] * config['m'] / (np.sqrt(config['v']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
